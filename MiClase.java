135e2a350fb43b93e0fc7b85f5bfa03787fe221a
joelviviant <joelviviant31@gmail.com>
2024-01-22 17:41:33 -0300

    initial commit

---
+++ MiClase.java
@@ -0,0 +1,17 @@
+public class MiClase{
+    public static void main(String[] args) {
+        int edad;
+        float altura, peso;
+        String nombre;
+
+        edad = 10;
+        altura = 175;
+        peso = 65;
+        nombre = "Arturo";
+        System.out.println(edad + " edad");
+        System.out.println(altura + " altura");
+        System.out.println(nombre + " nombre");
+        System.out.println(peso + " peso");
+    }
+    
+}
