f19f7c9540fd8c2edb1745827db6dc1172deaaaa
Joel Viviant <joelviviant31@gmail.com>
2024-01-23 17:36:25 -0300

    Clase 4 mas ejercicios

--- Clase4.java
+++ Clase4.java
@@ -4,8 +4,15 @@ import java.io.InputStreamReader;
 public  class Clase4 {
 
     public static void main(String[] args) {
-        ejercicio1();
+        //ejercicio1();
+        //ejercicio2();
+        //ejercicio3();
+        //ejercicio2_1();
+        //ejercicio2_2();
+        ejercicio2_3();
     }
+
+    //Parte 1 TP Clase 4 - Selectivas o Condicionales
     public static void ejercicio1() {
         char numero1;
         try {
@@ -13,14 +20,146 @@ public  class Clase4 {
             System.out.println("Ingrese un caracter :");
             String linea = entrada.readLine();
             if (linea != null && linea.length() > 0) {
-                numero1=linea.charAt(0);
-                if (numero1>='0' && numero1 <='9'){
+                numero1 = linea.charAt(0);
+                if (numero1 >= '0' && numero1 <= '9') {
                     System.out.println("El caracter es " + numero1);
-                }else{
+                } else {
                     System.out.println("El caracter ingresado no es un digito");
                 }
-            }else{
+            } else {
                 System.out.println("no se ha ingresado ningun digito");
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+
+    public static void ejercicio2() {
+        int numero1, numero2;
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingrese un numero entero :");
+            numero1 = Integer.parseInt(entrada.readLine());
+            System.out.println("Ingrese un numero entero :");
+            numero2 = Integer.parseInt(entrada.readLine());
+            if (numero1 > numero2) {
+                System.out.println("A");
+            } else if (numero1 % 2 == 0 && numero2 % 2 == 0) {
+                System.out.println("B");
+            } else {
+                System.out.println("C");
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    public static void ejercicio3() {
+        int numero1;
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingrese un numero entero :");
+            numero1 = Integer.parseInt(entrada.readLine());
+            if (numero1 % 6 == 0 && numero1 % 7 == 0) {
+                System.out.println("es multiplo de 6 y 7");
+            } else if (numero1 > 30 && numero1 % 2 == 0) {
+                System.out.println("es mayor a 30 y multiplo de 2");
+            }
+            if (numero1 / 5 > 10) {
+                System.out.println("el cociente de la división de dicho número con 5 es mayor a 10");
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    //Parte 2 TP Clase 4 - Selectivas o Condicionales
+
+    public static void ejercicio2_1() {
+        int numero1;
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingrese un numero entero :");
+            numero1 = Integer.parseInt(entrada.readLine());
+            if (numero1 > 0) {
+                if (numero1 >= 100) {
+                    System.out.println("grande");
+                } else {
+                    System.out.println("menor");
+                }
+                System.out.println("El valor es positivo");
+            } else {
+                if (numero1 % 2 == 0) {
+                    System.out.println("Es par");
+                } else if (numero1 % 3 == 0) {
+                    System.out.println("Es multiplo de 3");
+                } else {
+                    System.out.println("No es par y no es multiplo de 3");
+                }
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+
+    public static void ejercicio2_2() {
+        char char1;
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingrese un caracter :");
+            String linea = entrada.readLine();
+            if (linea != null && linea.length() > 0) {
+                char1 = linea.charAt(0);
+                if (char1 >= '0' && char1 <= '9') {
+                    System.out.println("Es un digito");
+                } else if (char1 >= 'a' && char1 <= 'z') {
+                    System.out.println("Es una letra minuscula");
+                    if (char1 == 'a' || char1 == 'e' || char1 == 'i' || char1 == 'o' || char1 == 'u') {
+                        System.out.println("Es una vocal");
+                    } else {
+                        System.out.println("Es una consonante");
+                    }
+                } else {
+                    System.out.println("No es un digito y no una letra minuscula");
+                }
+            } else {
+                System.out.println("No se ha ingresado ningun caracter");
+            }
+        } catch (Exception exc) {
+            System.out.println(exc);
+        }
+    }
+
+    public static void ejercicio2_3() {
+        int numero1;
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Ingrese un numero entero :");
+            numero1 = Integer.parseInt(entrada.readLine());
+
+            switch (numero1){
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12: {
+                    System.out.println("El mes tiene 31 dias");
+                    break;
+                }
+                case 2:{
+                    BufferedReader anioEntrada = new BufferedReader(new InputStreamReader(System.in));
+                    System.out.println("Ingrese un año :");
+                    int anio = Integer.parseInt(anioEntrada.readLine());
+                    if ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400 == 0))){
+                        System.out.println("El mes tiene 29");
+                    }else {
+                        System.out.println("El mes tiene 28");
+                    }
+                    break;
+                }
+                case 4: case 6: case 9: case 11:{
+                    System.out.println("El mes tiene 30 dias");
+                }
+                default:
+                    System.out.println("ese numero no corresponde a un mes");
             }
         } catch (Exception exc) {
             System.out.println(exc);
